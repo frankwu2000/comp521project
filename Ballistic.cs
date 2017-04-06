38de2f27428e97c6dd7e50fd71fd6302dbf5a117
hongyi wu <frankwu2000@hotmail.com>
2017-04-06 11:50:40 -0400

    comp521project
    -add physical particle
    -add ballistic

---
+++ Ballistic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballistic : MonoBehaviour {
+	public float mass; 
+	public float gravity;
+	public int ShootingMode; // ShootingMode: 0 - none, 1 - cannon, 2 - bullet
+	Particle particle ;
+
+	void Start () {
+//		particle = new Particle (mass, gravity, velocity, gameObject.transform.position);
+		if (ShootingMode == 0) {
+			particle = new Particle (mass, gravity, Vector3.zero, gameObject.transform.position);
+		} else if (ShootingMode == 1) {
+			particle = new Particle (200, gravity, new Vector3 (20, 15, 0), gameObject.transform.position);
+			particle.acceleration = new Vector3 (0, -20, 0);
+		} else if (ShootingMode == 2) {
+			particle = new Particle (2, gravity, new Vector3 (50, 0, 0), gameObject.transform.position);
+			particle.acceleration = new Vector3 (0, -1, 0);
+
+		}
+	}
+
+	void Update () {
+		UpdateParticle ();
+		if (Input.GetKey(KeyCode.Space)) {
+			particle.Integration (Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			particle.AddForce (new Vector3(0,0,300));
+			particle.Integration (Time.deltaTime);
+		}
+//		if (Input.GetKey(KeyCode.D)) {
+//			particle.AddForce (new Vector3(0,0,-300));
+//			//particle.Integration (Time.deltaTime);
+//		}
+//		particle.Integration (Time.deltaTime);
+
+	}
+
+	void UpdateParticle(){
+		gameObject.transform.position = particle.position;
+	}
+}
---
+++ Particle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Particle{
+	public float gravity;
+	public float mass;
+	float damping;
+	public Vector3 position;
+	public Vector3 velocity;
+	public Vector3 acceleration;
+
+	float inverseMass;
+	Vector3 gravityForce;
+	Vector3 forceAccum;
+
+	//constructor without force input
+	public Particle(float mass, float gravity, Vector3 velocity, Vector3 position){
+		this.position = position;
+		this.damping = 0.995f;
+		this.mass = mass;
+		this.inverseMass = 1 / mass;
+
+		this.gravityForce = new Vector3 (0, -gravity * mass, 0);
+		this.forceAccum = Vector3.zero;
+		forceAccum += gravityForce;
+
+		this.acceleration = Vector3.zero;
+		this.velocity = velocity;
+
+
+	}
+
+	//physics integration, duration is time to update one frame
+	public void Integration(float duration){
+		
+		//get acceleration from force by Newton second law , f = m*a so a = f*1/m
+		Vector3 resultAcc = acceleration;
+		resultAcc = resultAcc + forceAccum * inverseMass;
+
+		//update velocity from acceleration , v = v + a * time
+		velocity = velocity* Mathf.Pow(damping, duration) + resultAcc * duration;
+
+		// Impose drag force for newton first law
+		velocity = velocity * Mathf.Pow(damping, duration);
+
+		//update position , p = p + v * time
+		position += velocity * duration;
+
+		ClearForce ();
+
+	}
+
+	public void ClearForce(){
+		forceAccum = Vector3.zero;
+		forceAccum += gravityForce;
+	}
+
+	public void AddForce(Vector3 force){
+		forceAccum += force;
+	}
+
+
+}
